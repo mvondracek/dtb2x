836e8b1255c1c86d29c851cd84ce5392c4dd7a9d
Martin Vondráček <vondracek.mar@gmail.com>
2019-02-27 01:51:51 +0100

    fix: tests: assertEqual parameter order

--- test_dtb2csv.py
+++ test_dtb2csv.py
@@ -11,32 +11,32 @@ class TestDtbReader(TestCase):
 
     def test_read_one_player(self):
         group = self.reader.read('group_name - group_note\n')
-        self.assertEqual(group.name, 'group_name')
-        self.assertEqual(group.note, 'group_note')
+        self.assertEqual('group_name', group.name)
+        self.assertEqual('group_note', group.note)
 
         team = self.reader.read('	team_name - team_note\n')
-        self.assertEqual(team.name, 'team_name')
-        self.assertEqual(team.note, 'team_note')
-        self.assertEqual(team.group, group)
+        self.assertEqual('team_name', team.name)
+        self.assertEqual('team_note', team.note)
+        self.assertEqual(group, team.group)
 
         player = self.reader.read('		123456789 - player_surname player_name, 01.01.1900 , player_note\n')
-        self.assertEqual(player.name, 'player_name')
-        self.assertEqual(player.surname, 'player_surname')
-        self.assertEqual(player.date_of_birth, '01.01.1900')
-        self.assertEqual(player.registration_number, '123456789')
-        self.assertEqual(player.note, 'player_note')
-        self.assertEqual(player.team, team)
+        self.assertEqual('player_name', player.name)
+        self.assertEqual('player_surname', player.surname)
+        self.assertEqual('01.01.1900', player.date_of_birth)
+        self.assertEqual('123456789', player.registration_number)
+        self.assertEqual('player_note', player.note)
+        self.assertEqual(team, player.team)
 
     def test_read_more_players(self):
         group = self.reader.read('group_name - group_note\n')
         team = self.reader.read('	team_name - team_note\n')
 
         player = self.reader.read('		1 - player_surname player_name, 01.01.1900 , player_note\n')
-        self.assertEqual(player.team, team)
-        self.assertEqual(player.team.group, group)
+        self.assertEqual(team, player.team)
+        self.assertEqual(group, player.team.group)
         player = self.reader.read('		2 - player_surname player_name, 01.01.1900 , player_note\n')
-        self.assertEqual(player.team, team)
-        self.assertEqual(player.team.group, group)
+        self.assertEqual(team, player.team)
+        self.assertEqual(group, player.team.group)
 
     def test_read_more_teams(self):
         group = self.reader.read('group_name - group_note\n')
@@ -64,7 +64,7 @@ class TestConvert(TestCase):
             'player_note\n')
         output_file = io.StringIO(newline=None)
         convert(input_file, output_file)
-        self.assertEqual(output_file.getvalue(), output_expected)
+        self.assertEqual(output_expected, output_file.getvalue())
 
     def test_basic(self):
         input_file = io.StringIO(
@@ -78,7 +78,7 @@ class TestConvert(TestCase):
             'group_name;;team_name;;;player_name;;;\n')
         output_file = io.StringIO(newline=None)
         convert(input_file, output_file)
-        self.assertEqual(output_file.getvalue(), output_expected)
+        self.assertEqual(output_expected, output_file.getvalue())
 
     def test_empty(self):
         input_file = io.StringIO(
@@ -92,7 +92,7 @@ class TestConvert(TestCase):
             ';;;;;;;;\n')
         output_file = io.StringIO(newline=None)
         convert(input_file, output_file)
-        self.assertEqual(output_file.getvalue(), output_expected)
+        self.assertEqual(output_expected, output_file.getvalue())
 
     def test_partial(self):
         input_file = io.StringIO(
@@ -112,7 +112,7 @@ class TestConvert(TestCase):
             'group3;group_note;team3;team_note;123456789;player_name;player_surname;01.01.2000;player_note\n')
         output_file = io.StringIO(newline=None)
         convert(input_file, output_file)
-        self.assertEqual(output_file.getvalue(), output_expected)
+        self.assertEqual(output_expected, output_file.getvalue())
 
     def test_full(self):
         input_file = io.StringIO(
@@ -126,7 +126,7 @@ class TestConvert(TestCase):
             'group_name;group_note;team_name;team_note;123456789;player_name;player_surname;01.01.2000;player_note\n')
         output_file = io.StringIO(newline=None)
         convert(input_file, output_file)
-        self.assertEqual(output_file.getvalue(), output_expected)
+        self.assertEqual(output_expected, output_file.getvalue())
 
     def test_invalid_file_team_wo_group(self):
         input_file = io.StringIO(
